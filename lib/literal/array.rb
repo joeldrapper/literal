cfb2c47be348dee3ac82835e4b23111be60c79ee
Joel Drapper <joel@drapper.me>
2023-05-14 12:47:29 +0100

    Start working on Array

--- lib/literal/array.rb
+++ lib/literal/array.rb
@@ -1,12 +1,40 @@
 # frozen_string_literal: true
 
 class Literal::Array
+	include Enumerable
+
 	def initialize(value, type:)
 		@value = value
 		@type = type
 	end
 
-	attr_accessor :value, :type
+	attr_reader :value, :type
+	protected attr_writer :value
+
+	def each(&block)
+		@value.each(&block)
+	end
+
+	def safe_map(type = @type, &block)
+		Literal::Array.new(
+			@value.map(&block),
+			type:
+		)
+	end
+
+	def safe_map!(type = @type, &block)
+		@type = type
+
+		@value.map! do |item|
+			output = block.call(item)
+
+			if type === output
+				output
+			else
+				raise Literal::TypeError.expected(output, to_be_a: type)
+			end
+		end
+	end
 
 	def <<(item)
 		unless @type === item
@@ -15,5 +43,43 @@ class Literal::Array
 
 		@value << item
 		self
+	end
+
+	def dup
+		duplicate = super
+		duplicate.value = @value.dup
+		duplicate
+	end
+
+	def concat(other)
+		case other
+		when Literal::Array(@type)
+			@value.concat(other.value)
+		when Literal::_Array(@type)
+			@value.concat(other)
+		when Literal::Array
+			raise Literal::TypeError.expected(other, to_be_a: Literal::Array(@type))
+		when Array
+			raise Literal::TypeError.expected(other, to_be_a: Literal::_Array(@type))
+		else
+			raise ArgumentError
+		end
+	end
+
+	def ==(other)
+		case other
+		when Array
+			@value == other
+		when Literal::Array
+			@value == other.value
+		else
+			false
+		end
+	end
+
+	def +(other)
+		duplicate = dup
+		duplicate.concat(other)
+		duplicate
 	end
 end
--- test/literal/array.test.rb
+++ test/literal/array.test.rb
@@ -4,10 +4,45 @@ let def whatever = Literal::Array(Integer).new([1, 2, 3])
 
 describe "#<<" do
 	test "with valid item" do
-		expect((whatever << 4).value) == [1, 2, 3, 4]
+		whatever << 4
+		expect(whatever) == [1, 2, 3, 4]
 	end
 
 	test "with invalid item" do
 		expect { whatever << "1" }.to_raise Literal::TypeError
+	end
+end
+
+describe "dup" do
+	test do
+		array = Literal::Array(Integer).new([1, 2, 3])
+		duplicate = array.dup
+
+		array << 4
+
+		expect(duplicate) == [1, 2, 3]
+	end
+end
+
+describe "#+" do
+	test do
+		a = Literal::Array(Integer).new([1, 2, 3])
+		b = [4, 5, 6]
+
+		both = a + b
+
+		expect(both) == [1, 2, 3, 4, 5, 6]
+		expect(both).to_be_a Literal::Array
+	end
+end
+
+describe "#safe_map" do
+	test do
+		array = Literal::Array(Integer).new([1, 2, 3])
+		array.safe_map!(String) { |number| number.to_s }
+
+		expect(array) == ["1", "2", "3"]
+
+		assert(Literal::Array(String) === array)
 	end
 end
