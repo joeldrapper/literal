ee7100f1b060da31488b7f00a795c493ca263799
Joel Drapper <joel@drapper.me>
2023-06-12 14:42:14 +0100

    Fix sig

--- lib/literal/modifiers/sig.rb
+++ lib/literal/modifiers/sig.rb
@@ -3,12 +3,12 @@
 module Literal::Modifiers::Sig
 	def sig(method_name, **signature)
 		return method_name unless Literal::EXPENSIVE_TYPE_CHECKS
-		raise ArgumentError unless signature.any?
+		raise ArgumentError unless signature.size == 1
 
 		types = signature.keys[0]
 		return_type = signature.values[0]
 		keywords = types[-1].is_a?(Hash) ? types.pop : {}
-		original_method = method(method_name)
+		original_method = instance_method(method_name)
 
 		define_method(method_name) do |*a, **k, &b|
 			types.each_with_index do |type, index|
@@ -23,7 +23,7 @@ module Literal::Modifiers::Sig
 				end
 			end
 
-			output = original_method.call(*a, **k, &b)
+			output = original_method.bind(self).call(*a, **k, &b)
 
 			unless return_type === output
 				raise Literal::TypeError.expected(output, to_be_a: return_type)
