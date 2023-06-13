fc20bfb2bf5cd354bfef71d2bdb28a9551555c3c
Joel Drapper <joel@drapper.me>
2023-06-13 10:49:23 +0100

    Change default struct reader/writer

--- lib/literal/struct.rb
+++ lib/literal/struct.rb
@@ -3,7 +3,7 @@
 class Literal::Struct
 	extend Literal::StructAttributes
 
-	def self.attribute(name, type, special = nil, reader: true, writer: true, positional: false)
+	def self.attribute(name, type, special = nil, reader: :public, writer: :public, positional: false)
 		super(name, type, special, reader:, writer:, positional:)
 	end
 end
