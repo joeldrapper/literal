d09dca499e96231104feac31399e2d5d985c0248
Joel Drapper <joel@drapper.me>
2024-06-23 16:39:12 +0100

    Marshal data structures (#87)

--- lib/literal/data_structure.rb
+++ lib/literal/data_structure.rb
@@ -4,6 +4,12 @@
 class Literal::DataStructure
 	extend Literal::Properties
 
+	def self.from_pack(payload)
+		object = allocate
+		object.marshal_load(payload)
+		object
+	end
+
 	def ==(other)
 		if Literal::DataStructure === other
 			to_h == other.to_h
@@ -28,5 +34,23 @@ class Literal::DataStructure
 	def deconstruct_keys(keys)
 		h = to_h
 		keys ? h.slice(*keys) : h
+	end
+
+	def as_pack
+		marshal_dump
+	end
+
+	def marshal_load(payload)
+		_version, attributes, was_frozen = payload
+
+		attributes.each do |key, value|
+			instance_variable_set("@#{key}", value)
+		end
+
+		freeze if was_frozen
+	end
+
+	def marshal_dump
+		[1, to_h, frozen?]
 	end
 end
--- lib/literal/struct.test.rb
+++ lib/literal/struct.test.rb
@@ -33,3 +33,35 @@ test do
 
 	expect(a) != b
 end
+
+# Marshal doesn't work with anonymous classes
+class ::RootStruct < Literal::Struct
+	prop :name, String
+end
+
+test "marshalling" do
+	a = RootStruct.new(name: "Joel")
+	b = Marshal.load(Marshal.dump(a))
+
+	expect(b) == a
+end
+
+test "marshalling a frozen struct" do
+	a = RootStruct.new(name: "Joel")
+	a.freeze
+
+	b = Marshal.load(Marshal.dump(a))
+
+	expect(b) == a
+	expect(b).to_be(:frozen?)
+end
+
+test "as_pack/from_pack" do
+	a = RootStruct.new(name: "Joel")
+	a.freeze
+
+	b = RootStruct.from_pack(a.as_pack)
+
+	expect(b) == a
+	expect(b).to_be(:frozen?)
+end
