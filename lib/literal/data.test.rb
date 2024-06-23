e2730aeca5aaf0c144dcabf50ff294c884884cd2
Joel Drapper <joel@drapper.me>
2024-06-23 13:55:11 +0100

    More Literal::Data tests

--- lib/literal/data.test.rb
+++ lib/literal/data.test.rb
@@ -4,10 +4,33 @@ class Person < Literal::Data
 	prop :name, String
 end
 
-test do
-	person = Person.new(name: +"John")
+test "properties have readers by default" do
+	person = Person.new(name: "John")
 	expect(person.name) == "John"
+end
 
+test "data objects are frozen" do
+	person = Person.new(name: "John")
 	expect(person).to_be(:frozen?)
+end
+
+test "mutable attributes are duplicated and frozen" do
+	name = +"John"
+	person = Person.new(name:)
+
 	expect(person.name).to_be(:frozen?)
+	expect(person.name).not_to_equal?(name)
+end
+
+test "immutable attributes are not duplicated" do
+	name = "John"
+	person = Person.new(name:)
+
+	expect(person.name).to_be(:frozen?)
+	expect(person.name).to_equal?(name)
+end
+
+test "to_h" do
+	person = Person.new(name: "John")
+	expect(person.to_h) == { name: "John" }
 end
--- lib/literal/data_property.rb
+++ lib/literal/data_property.rb
@@ -2,6 +2,6 @@
 
 class Literal::DataProperty < Literal::Property
 	def generate_initializer_assign_value
-		"#{ivar_ref} = #{local_var_ref}.frozen? ? #{local_var_ref} : #{local_var_ref}.freeze"
+		"#{ivar_ref} = #{local_var_ref}.frozen? ? #{local_var_ref} : #{local_var_ref}.dup.freeze"
 	end
 end
