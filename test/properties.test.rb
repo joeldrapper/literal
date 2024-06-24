7a19f3d3f29e5c48f25731cfc74163d4bd629fa0
Stephen Ierodiaconou <stevegeek@gmail.com>
2024-06-24 12:17:48 +0200

    Some helper methods for introspection

--- test/properties.test.rb
+++ test/properties.test.rb
@@ -37,4 +37,37 @@ end
 test "default block" do
 	object = WithDefaultBlock.new
 	expect(object.block.call) == "Hello"
+
+	object = WithDefaultBlock.new { "World" }
+	expect(object.block.call) == "World"
+end
+
+test "properties are enumerable" do
+	props = Person.literal_properties
+	expect(props.size) == 2
+	expect(props.map(&:name)) == [:name, :age]
+end
+
+test "introspection" do
+	prop1, prop2 = *Person.literal_properties
+
+	expect(prop1.name) == :name
+	expect(prop1.type) == String
+	assert(prop1.positional?) { "Expected name to be kind :positional" }
+	refute(prop1.keyword?) { "Expected name to not be kind :keyword" }
+	refute(prop1.block?) { "Expected name to not be kind :&" }
+	refute(prop1.splat?) { "Expected name to not be bind :*" }
+	refute(prop1.double_splat?) { "Expected name to not be kind :**" }
+	assert(prop1.required?) { "Expected name to be required" }
+	refute(prop1.optional?) { "Expected name to not be optional" }
+
+	expect(prop2.name) == :age
+	expect(prop2.type) == Integer
+	assert(prop2.keyword?) { "Expected age to be kind :keyword" }
+	assert(prop2.required?) { "Expected age to be required" }
+
+	props = WithDefaultBlock.literal_properties
+	prop_block = props.first
+	assert(prop_block.block?) { "Expected block to be kind :&" }
+	assert(prop_block.optional?) { "Expected block to be optional" }
 end
