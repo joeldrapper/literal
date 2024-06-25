ee5f0edd71e694e14328c4782316845a7ad345a4
Joel Drapper <joel@drapper.me>
2024-06-25 12:00:02 +0100

    Update data_schema.rb

--- lib/literal/properties/data_schema.rb
+++ lib/literal/properties/data_schema.rb
@@ -2,7 +2,7 @@
 
 class Literal::Properties::DataSchema < Literal::Properties::Schema
 	def generate_initializer_body(buffer = +"")
-		buffer << "@literal_properties = properties = self.class.literal_properties\n"
+		buffer << "properties = self.class.literal_properties.properties_index\n"
 		generate_initializer_handle_properties(@sorted_properties, buffer)
 		buffer << "after_initialize if respond_to?(:after_initialize)\nfreeze\n"
 	end
