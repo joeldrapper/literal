70b2de9a270738fe6dc4dc990e8884836bea794e
Joel Drapper <joel@drapper.me>
2024-11-10 20:29:23 +0000

    Prepend a blank initializer for RubyMine

--- lib/literal/object.rb
+++ lib/literal/object.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class Literal::Object
+	module BlankInitializerForRubyMine
+		def initialize(*a, **k, &b)
+			nil
+		end
+	end
+
+	prepend BlankInitializerForRubyMine
+
 	extend Literal::Properties
 end
