dbc6bef957fb0fd38399029032fba0e627c7e2c9
Joel Drapper <joel@drapper.me>
2024-12-10 19:03:30 +0000

    Simplify Rails type registration

--- lib/literal/railtie.rb
+++ lib/literal/railtie.rb
@@ -2,20 +2,14 @@
 
 class Literal::Railtie < Rails::Railtie
 	initializer "literal.register_literal_enum_type" do
-		ActiveRecord::Type.register(:literal_enum) do |name, type:|
-			Literal::Rails::EnumType.new(type)
-		end
+		[ActiveRecord::Type, ActiveModel::Type].each do |registry|
+			registry.register(:literal_enum) do |name, type:|
+				Literal::Rails::EnumType.new(type)
+			end
 
-		ActiveRecord::Type.register(:literal_flags) do |name, type:|
-			Literal::Rails::FlagsType.new(type)
-		end
-
-		ActiveModel::Type.register(:literal_enum) do |name, type:|
-			Literal::Rails::EnumType.new(type)
-		end
-
-		ActiveModel::Type.register(:literal_flags) do |name, type:|
-			Literal::Rails::FlagsType.new(type)
+			registry.register(:literal_flags) do |name, type:|
+				Literal::Rails::FlagsType.new(type)
+			end
 		end
 	end
 end
