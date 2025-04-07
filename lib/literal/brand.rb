0fa6ce8cefecb3d6ca4903afcae4ef029c91db86
Joel Drapper <j.drapper@plane.com>
2025-04-07 11:08:58 +0100

    Prevent branding interned types

--- lib/literal/brand.rb
+++ lib/literal/brand.rb
@@ -1,14 +1,33 @@
 # frozen_string_literal: true
 
 class Literal::Brand
+	extend Literal::Types
+	include Literal::Types
+
+	# These types are alwys interned so branding doesn’t make sense.
+	Interned = _Union(
+		Integer,
+		Symbol,
+		_Boolean,
+		nil
+	)
+
 	def initialize(...)
-		@type = Literal::Types._Constraint(...)
+		@type = _Constraint(...)
+
+		if Literal.subtype?(@type, of: Interned)
+			raise ArgumentError.new("You can’t brand an interned object. Use a Literal::Value instead.")
+		end
+
 		@objects = ObjectSpace::WeakMap.new
 	end
 
 	def new(object)
-		@objects[object] = true
-		object
+		Literal.check(expected: @type, actual: object)
+
+		dup = object.dup
+		@objects[dup] = true
+		dup
 	end
 
 	alias_method :[], :new
--- test/brand.test.rb
+++ test/brand.test.rb
@@ -2,12 +2,45 @@
 
 UserID = Literal::Brand(String)
 
-test do
-	string = "123"
+test "branded strings" do
+	user_id = UserID.new("123")
 
-	refute UserID === string
+	assert UserID === user_id
+	assert_equal "123", user_id
 
-	UserID.new(string)
+	refute UserID === "123"
+end
 
-	assert UserID === string
+test "prevent interned types" do
+	assert_raises ArgumentError do
+		Literal::Brand(Integer)
+	end
+
+	assert_raises ArgumentError do
+		Literal::Brand(true)
+	end
+
+	assert_raises ArgumentError do
+		Literal::Brand(false)
+	end
+
+	assert_raises ArgumentError do
+		Literal::Brand(Literal::Types._Boolean)
+	end
+
+	assert_raises ArgumentError do
+		Literal::Brand(nil)
+	end
+
+	assert_raises ArgumentError do
+		Literal::Brand(Symbol)
+	end
+
+	assert_raises ArgumentError do
+		Literal::Brand(1)
+	end
+
+	assert_raises ArgumentError do
+		Literal::Brand(:two)
+	end
 end
