b6479ab6623d63f107ef8e7333f67c6cb67c8e26
Joel Drapper <j.drapper@plane.com>
2025-04-07 18:46:41 +0100

    Update interface_type.rb

--- lib/literal/types/interface_type.rb
+++ lib/literal/types/interface_type.rb
@@ -6,28 +6,31 @@ class Literal::Types::InterfaceType
 
 	def initialize(*methods)
 		raise Literal::ArgumentError.new("_Interface type must have at least one method.") if methods.size < 1
-		@methods = methods.freeze
-		@methods_set = methods.to_set.freeze
+		@methods = methods.to_set.freeze
 		freeze
 	end
 
-	attr_reader :methods, :methods_set
+	attr_reader :methods
 
 	def inspect
 		"_Interface(#{@methods.map(&:inspect).join(', ')})"
 	end
 
 	def ===(value)
-		@methods.all? { |m| value.respond_to?(m) }
+		@methods.each do |method|
+			return false unless value.respond_to?(method)
+		end
+
+		true
 	end
 
 	def >=(other)
 		case other
 		when Literal::Types::InterfaceType
-			@methods_set.subset?(other.methods_set)
+			@methods.subset?(other.methods)
 		when Module
 			public_methods = other.public_instance_methods.to_set
-			@methods_set.subset?(public_methods)
+			@methods.subset?(public_methods)
 		when Literal::Types::IntersectionType
 			other.types.any? { |type| Literal.subtype?(type, self) }
 		when Literal::Types::ConstraintType
