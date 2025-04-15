9c40afdebdf719586260e1e7aa44bb0f61f65151
Joel Drapper <j.drapper@plane.com>
2025-04-15 11:10:07 +0100

    More value tests

--- test/value.test.rb
+++ test/value.test.rb
@@ -6,6 +6,14 @@ Name = Literal::Value(String) do
 	delegate :length
 end
 
+class Email < Literal::Value(String)
+	delegate :length
+
+	def domain
+		value.split("@").last
+	end
+end
+
 test do
 	user_id = UserID.new(123)
 	assert_equal(123, user_id.to_i)
@@ -16,4 +24,16 @@ test do
 
 	name = Name.new("Joel")
 	assert_equal 4, name.length
+end
+
+test "class inheritance" do
+	email = Email.new("joel@drapper.me")
+
+	assert_equal email.domain, "drapper.me"
+	assert_equal email.length, 15
+	assert_equal email.to_s, "joel@drapper.me"
+	assert_equal email.to_str, "joel@drapper.me"
+
+	assert Email === email
+	refute String === email
 end
